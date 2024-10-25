ba002198cfb9c805af8ed6a471d046ce55261117
brscn2 <brscn55@yahoo.com>
2024-10-25 19:57:37 +0300

    Homework Phase 1 Implemented

--- code/receiver/icmp_receiver.py
+++ code/receiver/icmp_receiver.py
@@ -1,3 +1,20 @@
-import scapy
+from scapy.all import sniff, IP, ICMP
 
-# Implement your ICMP receiver here
+
+def packet_callback(packet):
+    # Check if packet has the ICMP layer
+    if packet.haslayer(ICMP):
+        # Check if the packet has TTL=1
+        if packet[IP].ttl == 1:
+            # Display the packet
+            packet.show()
+
+
+def start_sniffing():
+    # Wait for incoming packet, on packet arrival call packet_callback function
+    sniff(filter="icmp", prn=packet_callback, count=1)
+
+
+if __name__ == "__main__":
+    start_sniffing()
+    exit()
--- code/sender/icmp_sender.py
+++ code/sender/icmp_sender.py
@@ -1,3 +1,19 @@
-import scapy
+from scapy.all import IP, send, ICMP
+
 
 # Implement your ICMP sender here
+
+def send_icmp_packet():
+    # Create an IP packet
+    ip_packet = IP(dst="receiver", ttl=1)
+
+    # Add ICMP layer onto the ip_packet
+    icmp_packet = ip_packet / ICMP()
+
+    # Send the ICMP packet
+    send(icmp_packet, verbose=False)
+
+
+if __name__ == "__main__":
+    send_icmp_packet()
+    exit()
